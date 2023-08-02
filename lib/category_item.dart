093efd7e4e0e1dbcbb07310495b481b086c823a8
RasikaRD <rasikarajith94@gmail.com>
2023-08-02 23:37:12 +0530

    add meals recipes screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mealapp/category_meals_screen.dart';
 
 class CtegoryItem extends StatelessWidget {
   final String title;
@@ -6,26 +7,37 @@ class CtegoryItem extends StatelessWidget {
   const CtegoryItem(this.title, this.color, {super.key});
 
   // CtegoryItem(this.title, this.color)
+  void selectCategory(BuildContext ctx){
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
+      return const CategoryMealsScreen();
+    },
+   ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration:  ShapeDecoration(
-          color: color,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(15)))),
-      child: Text(title,
-      style: Theme.of(context).textTheme.titleMedium,),
-      // decoration: BoxDecoration(
-      //   gradient: LinearGradient(
-      //     colors: [color.withOpacity(0.7)],
-      //     begin: Alignment.topLeft,
-      //     end: Alignment.bottomCenter,
-      //   ),
-      //   borderRadius: BorderRadius.circular(15),
-      // ),
-      // child: Text(title),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration:  ShapeDecoration(
+            color: color,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15)))),
+        child: Text(title,
+        style: Theme.of(context).textTheme.titleMedium,),
+        // decoration: BoxDecoration(
+        //   gradient: LinearGradient(
+        //     colors: [color.withOpacity(0.7)],
+        //     begin: Alignment.topLeft,
+        //     end: Alignment.bottomCenter,
+        //   ),
+        //   borderRadius: BorderRadius.circular(15),
+        // ),
+        // child: Text(title),
+      ),
     );
   }
 }
